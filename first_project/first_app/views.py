65c163608783cd938c45096da3555cbea628f0fd
PRUDHVI <prudhvichowdary14@gmail.com>
2025-02-27 21:29:38 +0530

    started templates

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello World!")
-
+    my_dict = {'insert_me': "Hello, I am from views.py"}
+    return render(request, 'first_app/index.html', context=my_dict)
 def greetings(request):
     return HttpResponse("Welcome to my site!")
